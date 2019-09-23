a77c1d039e231c3dfedc438f52254bef77ae6124
ivebencrazy <ivebencrazy@users.noreply.github.com>
2019-09-22 20:18:11 -0700

    feat: adds immediate param to debounce

--- packages/utilities/source/debounce/debounce.ts
+++ packages/utilities/source/debounce/debounce.ts
@@ -1,15 +1,30 @@
 import { Func } from ".."
 
-
+/**
+ * Return a function that won't be triggered as long as it continues to be invoked
+ * @param func Function to be debounced
+ * @param wait Milliseconds wait time
+ * @param immediate Trigger `func` on leading edge instead of trailing
+ * @returns debounced function
+ */
 export function debounce(
   func: Func,
   wait: number,
+  immediate?: boolean,
 ): () => void {
   let timer: number
 
-  return () => {
-    if (timer) window.clearTimeout(timer)
+  return function debounced(...args: any[]) {
+    const later = () => {
+      window.clearTimeout(timer)
+      if (!immediate) func.apply(this, args)
+    }
 
-    timer = window.setTimeout(func, wait)
+    if (immediate && !timer) {
+      func.apply(this, args)
+    }
+
+    window.clearTimeout(timer)
+    timer = window.setTimeout(later, wait)
   }
 }
